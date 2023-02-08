6b31d8ef62f49197785f842c5fb44c879c94fecb
Petter Kraus <petterkraus@gmail.com>
2023-02-08 11:53:20 -0300

    CORs correction

--- src/service/API.js
+++ src/service/API.js
@@ -1,12 +1,5 @@
 import axios from "axios";
 
-// export default axios.create({
-//   baseURL: ' https://cors-anywhere.herokuapp.com/https://dropmail.me/api/graphql/',
-//   timeout: 10000,
-//   headers: { "Content-Type": "application/json",  'X-Requested-With': 'XMLHttpRequest'  },
-
-// });
-
 export default axios.create({
   baseURL: ' https://cors-anywhere.herokuapp.com/https://dropmail.me/api/graphql/',
   timeout: 10000,
